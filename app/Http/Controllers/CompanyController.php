bd719f975d1500f729d3aacd7845a52068d708f4
Syukron Rifail M <syukronrifai@gmail.com>
2021-04-05 11:16:58 +0700

    Add controllers

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CompanyController extends Controller
+{
+    /**
+     * @api {post} /api/companies/:id Get a company
+     * @apiGroup Company
+     * @apiDescription Get a company. This is the description.
+     * 
+     * @apiUse DefaultHeader
+     * @apiUse DefaultError
+     *
+     * @apiParamExample {cli} Request example:
+     *  curl https://example.com/api/companies/cad3cf57-105a-4647-a9dc-ddc52a6ec654
+     * 
+     * @apiSuccess id Company id
+     * @apiSuccess name Company name
+     * @apiSuccess location Company location
+     * 
+     * @apiSuccessExample {json} Response example:
+     *  {
+     *    "id": "cad3cf57-105a-4647-a9dc-ddc52a6ec654",
+     *    "name": "Shell Co. Ltd.",
+     *    "location": "Stockholm",
+     *  }
+     */
+    public function show()
+    {
+
+    }
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,75 @@ namespace App\Http\Controllers;
 
 class UserController extends Controller
 {
-    public function get()
+    /**
+     * @api {post} /api/users Create a user
+     * @apiGroup User
+     * @apiDescription Create a octg user. This is the description.
+     * 
+     * @apiUse DefaultHeader
+     * @apiUse DefaultError
+     * 
+     * @apiParam {String} name User name
+     * @apiParam {String} email User email
+     * @apiParamExample {json} Request example:
+     *  {
+     *    "email": "johndoe@example.com",
+     *    "name": "John Doe",
+     *  }
+     * 
+     * @apiSuccess id User id
+     * @apiSuccess name User name
+     * @apiSuccess email User email
+     * 
+     * @apiSuccessExample {json} Response example:
+     *  {
+     *    "id": "b47562348364badf",
+     *    "email": "johndoe@example.com",
+     *    "name": "John Doe",
+     *  }
+     */
+    public function create()
     {
         //
     }
 
-    public function create()
+    /**
+     * @api {get} /api/users Get users
+     * @apiGroup User
+     * @apiDescription Get all users
+     * 
+     * @apiUse DefaultHeader
+     * @apiUse DefaultError
+     * 
+     * @apiParam {String} [company] User company
+     * 
+     * @apiParamExample {cli} Request example:
+     *   curl http://example.com/api/users?company=shell
+     * 
+     * @apiSuccess {Integer} total Total data
+     * @apiSuccess {Object[]} data Users returned
+     * @apiSuccess {String} data.id User ID
+     * @apiSuccess {String} data.email User email
+     * @apiSuccess {String} data.name User name
+     * 
+     * @apiSuccessExample {json} Response example:
+     *  {
+     *      "total": 2,
+     *      "data": [
+     *          {
+     *              "id": "bc780f99-e004-4be3-a2dc-33658ac2f7cc",
+     *              "email": "johndoe@example.com",
+     *              "name": "John Doe"
+     *          },
+     *          {
+     *              "id": "6f5a32a2-cba2-4f1a-b43f-5c12c4b94dbf",
+     *              "email": "dave@example.com",
+     *              "name": "Dave"
+     *          }
+     *      ]
+     *  }
+     */
+    public function get()
     {
         //
     }
