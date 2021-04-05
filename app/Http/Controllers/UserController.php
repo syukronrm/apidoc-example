58aaa02eea6b44fb151ea9c48c761cb6f4d09d00
Syukron Rifail M <syukronrifai@gmail.com>
2021-04-05 10:18:24 +0700

    init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UserController extends Controller
+{
+    public function get()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+}
